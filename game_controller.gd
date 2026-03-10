fbbced2bf15fb5306d8beffdae85b32757628170
tortoise501 <tralala5.555.d@gmail.com>
2026-03-10 18:57:42 +0100

    enemies now spawn in batch and spawn rate increases with time

--- game_controller.gd
+++ game_controller.gd
@@ -6,9 +6,21 @@ var score = 0
 
 @onready var enemy_spawners = [$EnemySpawnerSW]
 
-#TEST ONLY
-var time_between_enemy_spawn = 5000 #msec
+#spawns / sec
+var start_spawn_rate = 0.2
+var max_spawn_rate = 4
+var time_to_max_spawnrate = 180 #sec
+func get_current_spawn_rate():
+	var increase_span = max_spawn_rate - start_spawn_rate
+	var time_passed = Time.get_ticks_msec() / 1000
+	var coof = min(1.0, time_passed / float(time_to_max_spawnrate))
+	return coof * increase_span
+	
+var enemies_queued = 0
+
 var last_time_enemy_spawn = -100000
+var time_between_enemy_spawn = 5000
+var last_time_enemy_queue = -100000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,8 +29,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var time_between_enemy_queue = (1 / get_current_spawn_rate()) * 1000
+	if last_time_enemy_queue + time_between_enemy_queue < Time.get_ticks_msec():
+		enemies_queued += 1
+		last_time_enemy_queue = Time.get_ticks_msec()
+		
 	if last_time_enemy_spawn + time_between_enemy_spawn < Time.get_ticks_msec():
-		enemy_spawners[0].spawn_enemy()
+		while enemies_queued > 0:
+			enemy_spawners[0].spawn_enemy()
+			enemies_queued -= 1
 		last_time_enemy_spawn = Time.get_ticks_msec()
 	pass
 
